5d64a34e7fe8202d94215eaddd87f32c34846fb3
Nandkishor <malinandkishor445@gmail.com>
2026-02-09 11:51:06 +0530

    feat: Update CORS configuration to include an additional local IP address

--- server/src/server.js
+++ server/src/server.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://10.230.12.74:3000", "http://10.154.153.74:3000", "http://192.168.1.5:3000"], // Added common local IP range just in case
+  origin: ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://10.230.12.74:3000", "http://10.154.153.74:3000", "http://192.168.1.5:3000", "http://10.147.156.74:3000"], // Added common local IP range just in case
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Explicitly added OPTIONS
   credentials: true
 }));
